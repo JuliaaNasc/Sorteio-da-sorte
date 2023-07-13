ba74750784a01235fb484f050c9e1b9ddfd977f6
Julia Nascimento <juliavcostan@gmail.com>
2023-07-13 17:24:41 -0300

    permitindo o usuariio colocar o valor mínimo e máximo

--- main.dart
+++ main.dart
@@ -2,26 +2,34 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  print('ESCOLHA UM NÚMERO MÁXIMO E VENHA PARTICIPAR DA NOSSA BRINCADEIRA: ');
+  print('VENHA PARTICIPAR DA NOSSA BRINCADEIRA... ');
+
+  print('ESCOLHA UM NÚMERO MÍNIMO: ');
+  String minimo = stdin.readLineSync() ?? '1';
+  int novoMinimo = int.parse(minimo);
+
+  print('AGORA ESCOLHA O VALOR MÁXIMO: ');
+  String maximo = stdin.readLineSync() ?? '1';
+  int novoMaximo = int.parse(maximo);
+
+  print('ME FALE UM VALOR COMO SUGESTÃO:  ');
   String sugestao = stdin.readLineSync() ?? '1';
+  int novaSugestao = int.parse(sugestao);
 
-  print('AGORA ESCOLHA O VALOR MÍNIMO: ');
-  String sugestao2;
-  sugestao2 = stdin.readLineSync() ?? '1';
+  int numero = Random().nextInt(novoMaximo + 1);
+  if (numero < novoMinimo) {
+    numero = novoMinimo;
+  }
 
-  // print('AGORA ESCOLHA UM VALOR MÍNINO: $sugestao');
-
-  int numero = Random().nextInt(101) + 1;
   print('O número escolhido foi: $numero');
 
-  int converter = int.parse(sugestao);
-  if (converter < numero) {
+  if (novaSugestao < numero) {
     print(
       'O número escolhido por você é menor do que o numero ESCOLHIDO no programa !!!',
     );
-  } else if (converter == numero) {
+  } else if (novaSugestao == numero) {
     print('VOCÊ DIGITOU O NÚMERO ESCOLHIDO!!!');
-  } else if (converter > numero) {
+  } else if (novaSugestao > numero) {
     print('o número que você digitou é maior que o número ESCOLHIDO !!!');
   }
 }
