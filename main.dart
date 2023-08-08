667c89341337009347568d2f41a2eff007200fee
Júlia Nascimento <juliavcostan@gmail.com>
2023-08-08 16:14:40 -0300

    Nessa nova atualização foi feito a pergunta pro usuario, se ele quer prosseguir com as sugestões do número que irá ser sorteado.

--- main.dart
+++ main.dart
@@ -14,46 +14,61 @@ void main() {
   String maximo = stdin.readLineSync() ?? '1';
   int novoMaximo = int.parse(maximo);
 
-  print('ME FALE UM VALOR EM NÚMERO COMO SUGESTÃO:  ');
-  String sugestao = stdin.readLineSync() ?? '1';
-  int novaSugestao = int.parse(sugestao);
- 
-  print('');
-
-  if (novaSugestao < novoMinimo) {
-    print(
-        'O NÚMERO QUE O USUÁRIO ESCOLHEU, É MENOR DO QUE O NÚMERO ESTABELECIDO COMO MINÍMO');
-        print('-----------------------------------------------------');
-        print('SEU CODÍGO NÃO RODOU POR UM ERRO NO VALOR ESTABELECIDO COMO SUGESTÃO!');
-        print('');
-  } else if (novaSugestao > novoMaximo) {
-    print(
-        'O NÚMERO QUE O USUÁRIO ESCOLHEU, É MAIOR DO QUE O NÚMERO ESTABELECIDO COMO MAXÍMO');
-       print('-----------------------------------------------------');
-       print('SEU CODÍGO NÃO RODOU POR UM ERRO NO VALOR ESTABELECIDO COMO SUGESTÃO!');
-       print('');
-
-  } else {
-    int numero = Random().nextInt(novoMaximo + 1);
-    if (numero < novoMinimo) {
-      numero = novoMinimo;
-    }
-
-    print('O número escolhido foi: $numero');
-    print('-----------------------------------------------------');
-
-    if (novaSugestao < numero) {
-      print(
-        'O número escolhido por você é menor do que o numero ESCOLHIDO no programa !!!',
-      );
-    } else if (novaSugestao == numero) {
-      print('VOCÊ DIGITOU O NÚMERO ESCOLHIDO!!!');
-    } else if (novaSugestao > numero) {
-      print('o número que você digitou é maior que o número ESCOLHIDO !!!');
-    }
-    print('-----------------------------------------------------');
-    print ('SEU CÓDIGO RODOU BEM, CASO PREFERIR, TENTE NOVAMENTE!');
-    print('');
+  int sorteado = Random().nextInt(novoMaximo + 1);
+  if (sorteado < novoMinimo) {
+    sorteado = novoMinimo;
   }
+  bool acertou = false;
+  bool querContinuar = true;
 
+  while (!acertou && querContinuar) {
+    print('ME FALE UM NOVO VALOR EM NÚMERO COMO SUGESTÃO:  ');
+    String sugestao = stdin.readLineSync() ?? '1';
+    int novaSugestao = int.parse(sugestao);
+
+    print('');
+
+    if (novaSugestao < novoMinimo) {
+      print(
+          'O NÚMERO QUE O USUÁRIO ESCOLHEU, É MENOR DO QUE O NÚMERO ESTABELECIDO COMO MINÍMO');
+      print('-----------------------------------------------------');
+      print(
+          'SEU CODÍGO NÃO RODOU POR UM ERRO NO VALOR ESTABELECIDO COMO SUGESTÃO!');
+      print('');
+    } else if (novaSugestao > novoMaximo) {
+      print(
+          'O NÚMERO QUE O USUÁRIO ESCOLHEU, É MAIOR DO QUE O NÚMERO ESTABELECIDO COMO MAXÍMO');
+      print('-----------------------------------------------------');
+      print(
+          'SEU CODÍGO NÃO RODOU POR UM ERRO NO VALOR ESTABELECIDO COMO SUGESTÃO!');
+      print('');
+    } else {
+      if (novaSugestao == sorteado) {
+        print('O número escolhido foi: $sorteado');
+        print('-----------------------------------------------------');
+
+        print('VOCÊ DIGITOU O NÚMERO ESCOLHIDO!!!');
+        acertou = true;
+      } else {
+        if (novaSugestao < sorteado) {
+          print(
+            'O número escolhido por você é menor do que o numero ESCOLHIDO no programa !!!',
+          );
+        } else if (novaSugestao > sorteado) {
+          print('o número que você digitou é maior que o número ESCOLHIDO !!!');
+        }
+
+        print('VOCÊ DESEJA CONTINUAR? (sim/ não)');
+        String resposta = stdin.readLineSync() ?? 'NÃO';
+
+        if (resposta != 'sim') {
+          querContinuar = false;
+        }
+      }
+
+      print('-----------------------------------------------------');
+      print('SEU CÓDIGO RODOU BEM, CASO PREFERIR, TENTE NOVAMENTE!');
+      print('');
+    }
+  }
 }
