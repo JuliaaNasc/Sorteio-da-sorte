9dd73afaefbfd3fc177a11317b62da17dbcdbe8e
Júlia Nascimento <juliavcostan@gmail.com>
2023-08-08 14:02:07 -0300

    foi estabelecida a pausa do código caso o usuario digitar um número fora da quantidade estabelecida como MAIOR OU MENOR, tambem foi estabelecido um frase para diferentes finalizaçóes

--- main.dart
+++ main.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 
 void main() {
   print('VENHA PARTICIPAR DA NOSSA BRINCADEIRA... ');
-  print ('___________________________________________');
+  print('-----------------------------------------------------');
   print('');
 
   print('ESCOLHA UM NÚMERO MÍNIMO: ');
@@ -17,29 +17,43 @@ void main() {
   print('ME FALE UM VALOR EM NÚMERO COMO SUGESTÃO:  ');
   String sugestao = stdin.readLineSync() ?? '1';
   int novaSugestao = int.parse(sugestao);
+ 
+  print('');
 
-  if( novaSugestao < novoMinimo) {
-    print ('O NÚMERO QUE O USUÁRIO ESCOLHEU, É MENOR DO QUE O NÚMERO ESTABELECIDO COMO MINÍMO');
-
-  } else if (novaSugestao > novoMaximo) {
-    print ('O NÚMERO QUE O USUÁRIO ESCOLHEU, É MAIOR DO QUE O NÚMERO ESTABELECIDO COMO MAXÍMO');
-
-  }
-
-  int numero = Random().nextInt(novoMaximo + 1);
-  if (numero < novoMinimo) {
-    numero = novoMinimo;
-  }
-
-  print('O número escolhido foi: $numero');
-
-  if (novaSugestao < numero) {
+  if (novaSugestao < novoMinimo) {
     print(
-      'O número escolhido por você é menor do que o numero ESCOLHIDO no programa !!!',
-    );
-  } else if (novaSugestao == numero) {
-    print('VOCÊ DIGITOU O NÚMERO ESCOLHIDO!!!');
-  } else if (novaSugestao > numero) {
-    print('o número que você digitou é maior que o número ESCOLHIDO !!!');
+        'O NÚMERO QUE O USUÁRIO ESCOLHEU, É MENOR DO QUE O NÚMERO ESTABELECIDO COMO MINÍMO');
+        print('-----------------------------------------------------');
+        print('SEU CODÍGO NÃO RODOU POR UM ERRO NO VALOR ESTABELECIDO COMO SUGESTÃO!');
+        print('');
+  } else if (novaSugestao > novoMaximo) {
+    print(
+        'O NÚMERO QUE O USUÁRIO ESCOLHEU, É MAIOR DO QUE O NÚMERO ESTABELECIDO COMO MAXÍMO');
+       print('-----------------------------------------------------');
+       print('SEU CODÍGO NÃO RODOU POR UM ERRO NO VALOR ESTABELECIDO COMO SUGESTÃO!');
+       print('');
+
+  } else {
+    int numero = Random().nextInt(novoMaximo + 1);
+    if (numero < novoMinimo) {
+      numero = novoMinimo;
+    }
+
+    print('O número escolhido foi: $numero');
+    print('-----------------------------------------------------');
+
+    if (novaSugestao < numero) {
+      print(
+        'O número escolhido por você é menor do que o numero ESCOLHIDO no programa !!!',
+      );
+    } else if (novaSugestao == numero) {
+      print('VOCÊ DIGITOU O NÚMERO ESCOLHIDO!!!');
+    } else if (novaSugestao > numero) {
+      print('o número que você digitou é maior que o número ESCOLHIDO !!!');
+    }
+    print('-----------------------------------------------------');
+    print ('SEU CÓDIGO RODOU BEM, CASO PREFERIR, TENTE NOVAMENTE!');
+    print('');
   }
+
 }
